52f7ec8ac44592a6c856e7b088cc36d0b92c7f59
Muhammad Junaid Iqbal <cuilahore147@gmail.com>
2021-07-26 00:05:23 +0500

    add hompage of initial stage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./homepage.styles.scss";
+import "./homepage.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">Books</div>
+            <span className="subtitle">Maths</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">Books</div>
+            <span className="subtitle">Maths</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">Books</div>
+            <span className="subtitle">Maths</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">Books</div>
+            <span className="subtitle">Maths</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">Books</div>
+            <span className="subtitle">Maths</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
