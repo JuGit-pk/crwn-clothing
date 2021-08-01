1d5b450bde0ea59883fb7aa1be9916cce3c6a256
Muhammad Junaid Iqbal <cuilahore147@gmail.com>
2021-08-01 16:24:57 +0500

    add Navigation bar or Header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Route exact path={"/"} component={HomePage} />
-      <Route exact path={"/shop"} component={Shop} />
+    <div className="app">
+      <Header />
+      <Switch>
+        <Route exact path={"/"} component={HomePage} />
+        <Route exact path={"/shop"} component={Shop} />
+      </Switch>
     </div>
   );
 }
